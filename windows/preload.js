2002bf2d815e472b9fcd3af6d131522e276672bc
kim hyeona <113571225+kim-hyeona@users.noreply.github.com>
2026-09-15 01:06:59 +0900

    날씨 및 노션 대시보드 연결 추가

--- windows/preload.js
+++ windows/preload.js
@@ -2,9 +2,10 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('hyeona', {
   load: (token) => ipcRenderer.invoke('dashboard:load', token),
+  loadWeather: (latitude, longitude) => ipcRenderer.invoke('weather:load', latitude, longitude),
+  openNotionDashboard: (token) => ipcRenderer.invoke('notion:dashboard-page', token),
   toggleTask: (token, id, done) => ipcRenderer.invoke('task:toggle', token, id, done),
   toggleRoutine: (token, id, done) => ipcRenderer.invoke('routine:toggle', token, id, done),
-  toggleBleeding: (token, id, done) => ipcRenderer.invoke('bleeding:toggle', token, id, done),
   saveBrainDump: (token, text) => ipcRenderer.invoke('braindump:save', token, text),
   saveBrainDraft: (text) => ipcRenderer.invoke('braindump:draft', text),
   createItem: (token, type, payload) => ipcRenderer.invoke('item:create', token, type, payload),
@@ -12,5 +13,5 @@ contextBridge.exposeInMainWorld('hyeona', {
   deleteItem: (token, id) => ipcRenderer.invoke('item:delete', token, id),
   openLink: (url) => ipcRenderer.invoke('link:open', url),
   getSettings: () => ipcRenderer.invoke('settings:get'),
-  setSettings: (s) => ipcRenderer.invoke('settings:set', s),
+  setSettings: (settings) => ipcRenderer.invoke('settings:set', settings),
 });
